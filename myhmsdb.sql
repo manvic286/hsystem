0c08e75f5a55171dc34b7c4c84aaea1f9ba55be4
Manuel404-dev <manuelviotor@gmail.com>
2023-04-24 04:30:30 +0000

    Few fixes

---
+++ myhmsdb.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2023 at 02:14 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myhmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admintb`
+--
+
+CREATE TABLE `admintb` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admintb`
+--
+
+INSERT INTO `admintb` (`username`, `password`) VALUES
+('admin', 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointmenttb`
+--
+
+CREATE TABLE `appointmenttb` (
+  `pid` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `doctor` varchar(30) NOT NULL,
+  `docFees` int(5) NOT NULL,
+  `appdate` date NOT NULL,
+  `apptime` time NOT NULL,
+  `userStatus` int(5) NOT NULL,
+  `doctorStatus` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `name` varchar(10) CHARACTER SET latin1 NOT NULL,
+  `email` text CHARACTER SET latin1 NOT NULL,
+  `message` varchar(200) CHARACTER SET latin1 NOT NULL,
+  `contact` varchar(10) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctb`
+--
+
+CREATE TABLE `doctb` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `spec` varchar(50) NOT NULL,
+  `docFees` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patreg`
+--
+
+CREATE TABLE `patreg` (
+  `pid` int(11) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `cpassword` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patreg`
+--
+
+INSERT INTO `patreg` (`pid`, `fname`, `lname`, `gender`, `email`, `contact`, `password`, `cpassword`) VALUES
+(10, 'Peter', 'Norvig', 'Male', 'peter@gmail.com', '9609362815', 'peter123', 'peter123'),
+(13, 'Manuel', 'Viotor', 'Male', 'manuelviotor@gmail.com', '0597380291', '123456', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prestb`
+--
+
+CREATE TABLE `prestb` (
+  `doctor` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `pid` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `fname` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `lname` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `appdate` date NOT NULL,
+  `apptime` time NOT NULL,
+  `disease` varchar(250) CHARACTER SET latin1 NOT NULL,
+  `allergy` varchar(250) CHARACTER SET latin1 NOT NULL,
+  `prescription` varchar(1000) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointmenttb`
+--
+ALTER TABLE `appointmenttb`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `patreg`
+--
+ALTER TABLE `patreg`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointmenttb`
+--
+ALTER TABLE `appointmenttb`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `patreg`
+--
+ALTER TABLE `patreg`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
